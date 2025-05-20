b38cbec2c47a15c86c923e8b36fefbaba193afa3
Mr.Strange <43387368+crazyrahul@users.noreply.github.com>
2025-05-20 10:00:44 +0530

    change code in app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
     
-
+app.get("/",(req,res)=>{
+    res.send("This is the Home Page");
+}
+);
 
 app.get("/user",(req,res)=>{
     res.send("This is the get method");
@@ -11,10 +14,7 @@ app.post("/user",(req,res)=>{
     res.send("This is the post method");
 });
 
-app.use("/user",(req,res)=>{
-    res.send("This is the use   method");
-}
-);
+
 
 app.listen(4000,(req,res)=>{
     console.log("Server is listening on port no 4000");
