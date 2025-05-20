936ead3d23f377107f9149026e59794de92b6bdd
Mr.Strange <43387368+crazyrahul@users.noreply.github.com>
2025-05-20 09:48:22 +0530

    created the routes

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
+    
 
 
-
-app.use("/hello",(req,res)=>
-{
-    res.send("This is Hello Page");
+app.get("/user",(req,res)=>{
+    res.send("This is the get method");
+}
+);
+app.post("/user",(req,res)=>{
+    res.send("This is the post method");
 });
-app.use((req,res)=>{
-    res.send("This is the Dashboard");
-});
 
-app.use("/test", (req,res)=>{
-    console.log("this is the test page");
-});
+app.use("/user",(req,res)=>{
+    res.send("This is the use   method");
+}
+);
 
 app.listen(4000,(req,res)=>{
     console.log("Server is listening on port no 4000");
